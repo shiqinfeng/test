499b70f55ab73f659c365bd5a3d851378a212f3c
shiqinfeng <afz6180@163.com>
2020-10-26 16:06:27 +0800

    测试MQ生产者与消费着

---
+++ ActiveMq/src/activeMq/Receiver.java
@@ -0,0 +1,66 @@
+package activeMq;
+
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MapMessage;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Receiver {
+	public static void main(String[] args) {
+		// ConnectionFactory ：连接工厂，JMS 用它创建连接
+		ConnectionFactory connectionFactory;
+		// Connection ：JMS 客户端到JMS Provider 的连接
+		Connection connection = null;
+		// Session： 一个发送或接收消息的线程
+		Session session;
+		// Destination ：消息的目的地;消息发送给谁.
+		Destination destination;
+		// 消费者，消息接收者
+		MessageConsumer consumer;
+		connectionFactory = new ActiveMQConnectionFactory(
+				ActiveMQConnection.DEFAULT_USER,
+				ActiveMQConnection.DEFAULT_PASSWORD, "tcp://localhost:61616");
+		try {
+			// 构造从工厂得到连接对象
+			connection = connectionFactory.createConnection();
+			// 启动
+			connection.start();
+			// 获取操作连接
+			session = connection.createSession(Boolean.FALSE,
+					Session.AUTO_ACKNOWLEDGE);
+			// 获取session注意参数值xingbo.xu-queue是一个服务器的queue，须在在ActiveMq的console配置
+			destination = session.createQueue("FirstQueue");
+			consumer = session.createConsumer(destination);
+			// consumer2 = session.createConsumer(destination);
+			System.out.println("消费者1：消费者程序开始运行...");
+			while (true) {
+				// 设置接收者接收消息的时间，为了便于测试，这里谁定为100s
+				TextMessage message = (TextMessage) consumer.receive(18000);
+				// Object message = (Object) consumer.receive(28000);
+				if (null != message) {
+					System.out.println("消费者1：收到消息-->" + message.getText());
+				} else {
+					System.out.println("消费者1：运行结束...\n");
+					break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != connection)
+					connection.close();
+			} catch (Throwable ignore) {
+			}
+		}
+	}
+}
